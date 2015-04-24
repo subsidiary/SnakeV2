169f32076654974bc74e4a65098791a4d6abd41f
subsidiary <yuriy.rogovtsov.2000@gmai.com>
2015-04-24 17:37:08 +0300

    StartPoint

--- app/src/main/java/com/snakev2v42/tiny/snakev2/GameActivity.java
+++ app/src/main/java/com/snakev2v42/tiny/snakev2/GameActivity.java
@@ -4,6 +4,36 @@ package com.snakev2v42.tiny.snakev2;
  * Created by yuriy on 4/24/2015.
  */
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.DisplayMetrics;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class GameActivity extends Activity {
+    static RelativeLayout ll;
+    static ImageView BoBar;
+    static TextView score;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_activity);loading();
+    }
+
+    public void loading(){
+        //Working on Graphic stuff
+        ll=(RelativeLayout)findViewById(R.id.gamelay);
+        BoBar=(ImageView)findViewById(R.id.BottomBar);
+        score=(TextView)findViewById(R.id.score);
+
+        DisplayMetrics display = this.getResources().getDisplayMetrics();
+        Values val = new Values(display.widthPixels,display.heightPixels,getResources().getDisplayMetrics().density);
+
+        BoBar.getLayoutParams().height=val.BoBaHeight;
+        score.setTextSize(val.BoBaHeight / val.dens - 4);
+    }
 }
--- app/src/main/java/com/snakev2v42/tiny/snakev2/Values.java
+++ app/src/main/java/com/snakev2v42/tiny/snakev2/Values.java
@@ -4,8 +4,17 @@ package com.snakev2v42.tiny.snakev2;
  * Created by yuriy on 4/24/2015.
  */
 public class Values {
-    static int CellWidth=40,CellHeight=21;
-    public Values(){
+    static int CellWidth,CellHeight,Width,Height,SnakeSize,BoBaHeight;
+    static float dens;
+    static boolean FirstLoad=true;
+    static int delayTime=230;
+    static int AMOUNT_OF_SNAKES;
+    static boolean game=true;
 
+    public Values(int width,int height,float density){
+        Width=width;Height=height;dens=density;
+        CellWidth=40;CellHeight=(int)((Height-30*dens)/Width*CellWidth);
+        SnakeSize=Width/CellWidth;
+        BoBaHeight=Height-CellHeight*SnakeSize;
     }
 }

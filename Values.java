c0792e119bfd46c31d505737ab00e97421b5faa3
subsidiary <yuriy.rogovtsov.2000@gmai.com>
2015-04-24 16:34:40 +0300

    Begining

---
+++ Values.java
@@ -0,0 +1,11 @@
+package com.snakev2v42.tiny.snakev2;
+
+/**
+ * Created by yuriy on 4/24/2015.
+ */
+public class Values {
+    static int CellWidth=40,CellHeight=21;
+    public Values(){
+
+    }
+}
